17bddc39382df76f3eeec8fd6c2310059057498e
Amy Duke <amyyyduke@gmail.com>
2019-08-25 15:09:52 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+
+
+
+
+def position_taken?(board, index)
+  if board[index] == nil || board[index] ==""
+      return false
+  elsif board[index] == " "
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end 
