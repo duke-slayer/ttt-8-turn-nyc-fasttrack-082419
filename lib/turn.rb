e661c725e3273ff437f20ac1ec20959304a8fe7b
Amy Duke <amyyyduke@gmail.com>
2019-08-25 15:10:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,12 @@
+def valid_move?(board, index)
+  if board[index] == nil
+    return false 
+  elsif !position_taken?(board, index)
+    return true 
+  end
+end 
 
-
-
-
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == nil || board[index] ==""
       return false
